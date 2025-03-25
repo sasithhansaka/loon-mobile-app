7917ac9434d343f9062a3ad371999340e32bfbbd
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-03-26 00:47:50 +0530

    Added Home Screen with Bottom Navigation Bar

---
+++ app/src/main/java/com/cns/loon/ui/homeScreen/HomeScreen.kt
@@ -0,0 +1,109 @@
+package com.cns.loon.ui.homeScreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+class HomeScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { innerPadding ->
+        NavigationHost(navController, Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Search,
+        BottomNavItem.Profile
+    )
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        item.icon,
+                        contentDescription = item.title,
+                        tint = Color.Black
+                    )
+                },
+                label = { Text(item.title) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun NavigationHost(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(navController, startDestination = BottomNavItem.Home.route, modifier = modifier) {
+        composable(BottomNavItem.Home.route) { HomeFragment(navController) }
+        composable(BottomNavItem.Search.route) {
+            SearchFragment(keyword = "")
+        }
+        composable(
+            route = "search?keyword={keyword}",
+            arguments = listOf(navArgument("keyword") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val keyword = backStackEntry.arguments?.getString("keyword") ?: ""
+            SearchFragment(keyword = keyword)
+        }
+        composable(BottomNavItem.Profile.route) {ProfileFragment() }
+    }
+
+}
+
+sealed class BottomNavItem(val title: String, val icon: ImageVector, val route: String) {
+    data object Home : BottomNavItem("Home", Icons.Filled.Home, "home")
+    data object Search : BottomNavItem("Search", Icons.Filled.Search, "search")
+    data object Profile : BottomNavItem("Profile", Icons.Filled.Person, "profile")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
